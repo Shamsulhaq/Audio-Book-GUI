ba23cb29eedf9fed2019a6392303da9e557e6d51
Shamsulhaq <bmshamsulhaq65@gmail.com.com>
2020-07-16 19:16:19 +0600

    Add menu and Icon

--- Src/main.py
+++ Src/main.py
@@ -3,16 +3,13 @@ import sys
 import pyttsx3
 import PyPDF2
 from PyQt5 import QtWidgets
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import *
 
 
-# book_name = open('python_tutorial.pdf', 'rb')  # open PDF file
-# pdfReader = PyPDF2.PdfFileReader(book_name)  # init PyPDF2 by PDF
-# speaker = pyttsx3.init()  # init pyttsx3
-# pages = pdfReader.numPages  # get total pages
 
-class Window(QWidget):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.left = 240
@@ -23,18 +20,60 @@ class Window(QWidget):
         self.setGeometry(self.left, self.top, self.widht, self.height)
         self.setWindowIcon(QIcon("Python_PyQt5.png"))  # Windows Icon
         self.setWindowTitle("AudioBook V02")
+        self.player_widget = Window()
+        self.setCentralWidget(self.player_widget)
+        self.show()
+        self.creatMenu()
 
+    def creatMenu(self):
+        # Creat Menu
+        bar = self.menuBar()
+        fileMenu = bar.addMenu("File")
+        helpMenu = bar.addMenu("Help")
+
+        # Create Actions
+
+        opneFile = QAction(QIcon("open_file.png"), "Open PDF", self)
+        opneFile.setShortcut("Ctrl+N")
+        opneFile.triggered.connect(self.player_widget.open_file)
+        fileMenu.addAction(opneFile)
+
+        closeApplication = QAction(QIcon("exit.png"), "Exit", self)
+        closeApplication.setShortcut("Ctrl+Q")
+        closeApplication.triggered.connect(self.exitApplication)
+        fileMenu.addAction(closeApplication)
+
+        aboutApplication = QAction(QIcon("about.png"), "About App", self)
+        aboutApplication.triggered.connect(self.player_widget.aboutMessage)
+        helpMenu.addAction(aboutApplication)
+        # toolbar Area
+        toolbar = self.addToolBar("Toolbar")
+        toolbar.addAction(opneFile)
+        toolbar.addAction(closeApplication)
+        toolbar.addAction(aboutApplication)
+
+    def exitApplication(self):
+        self.close()
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
         self.initWindow()
-        self.show()  # Show window
 
     def initWindow(self):
         # ============= Design component create  ============
         self.filename_label = QLabel("Open A PDF File First")  # Creat Label
+        self.filename_label.setStyleSheet("color: green")
+        self.filename_label.setFont(QFont("Arial",20))
         self.total_page_label = QLabel(" ")
+        self.total_page_label.setStyleSheet("color: blue")
+        self.total_page_label.setFont(QFont("Arial", 14))
         self.editor = QTextEdit()
-        self.aboutButton = QPushButton("About")  # Creat Button
-        self.playButton = QPushButton("Play")
-        self.openButton = QPushButton("Open")
+        # self.aboutButton = QPushButton("About")  # Creat Button
+        self.playButton = QPushButton(QIcon("play.png"),"Play")
+        self.playButton.setFont(QFont("Arial", 15))
+        # self.openButton = QPushButton("Open")
         # self.pauseButton = QPushButton("Pause")
         self.start_page = QLineEdit()  # Create Text input box
         self.end_page = QLineEdit()
@@ -47,10 +86,10 @@ class Window(QWidget):
 
         # set play pause and open button in Horizontal box
         self.h_layout = QHBoxLayout()
-        self.h_layout.addWidget(self.openButton)
+        # self.h_layout.addWidget(self.openButton)
         # self.h_layout.addWidget(self.pauseButton)
         self.h_layout.addWidget(self.playButton)
-        self.h_layout.addWidget(self.aboutButton)
+        # self.h_layout.addWidget(self.aboutButton)
 
         # Vertical box layout as Main layout
         self.v_layout = QVBoxLayout()  # Create Vertical box layout
@@ -63,16 +102,10 @@ class Window(QWidget):
         self.setLayout(self.v_layout)  # set Layout
 
         # connect all functional buttons
-        self.aboutButton.clicked.connect(self.aboutMessage)
+        # self.aboutButton.clicked.connect(self.aboutMessage)
         self.playButton.clicked.connect(self.play_file)
-        self.openButton.clicked.connect(self.open_file)
+        # self.openButton.clicked.connect(self.open_file)
         # self.pauseButton.clicked.connect(self.pause_file)
-
-
-
-
-
-
 
     # ======== button action functions ===================
     # def clear_file(self):
@@ -82,7 +115,7 @@ class Window(QWidget):
     def play_file(self):
         start = int(self.start_page.text()) or 1
         end = int(self.pages) or int(self.end_page.text())
-        self.filename_label.setText("Playing::"+ self.filename_label.text())
+        self.filename_label.setText("Playing::" + self.filename_label.text())
 
         for num in range(start, self.pages):
             page = self.pdfReader.getPage(num)  # get page
@@ -91,24 +124,24 @@ class Window(QWidget):
             self.speaker.say(text)  # speck text
             self.speaker.runAndWait()
 
-
     def open_file(self):
         file_name = QFileDialog.getOpenFileName(self, "Open File", "C://", "PDF files (*.pdf) ;;All Files()")
-        self.book_name = open(file_name[0],'rb')  # open PDF file
+        self.book_name = open(file_name[0], 'rb')  # open PDF file
         self.pdfReader = PyPDF2.PdfFileReader(self.book_name)  # init PyPDF2 by PDF
         self.speaker = pyttsx3.init()  # init pyttsx3
         self.pages = self.pdfReader.numPages  # get total pages
         self.total_page_label.setText("Total Pages: " + str(self.pages))
         head, tail = os.path.split(file_name[0])
         self.filename_label.setText(tail)
+        self.start_page.setText("1")
+        self.end_page.setText(str(self.pages))
+        # self.show()  # Show window
 
     def aboutMessage(self):
         message = QMessageBox.about(self, "About AudioBook",
                                     "This is PDF Voice Reader \n Version: 1.0.1 \n Build: 02 \n Develop by: Shamsul Haq")
 
 
-
-
 app = QtWidgets.QApplication(sys.argv)
-a_window = Window()
+window = MainWindow()
 sys.exit(app.exec_())  # exit Window
